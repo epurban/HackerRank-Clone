ddf8bf53beb7db44be13bcf7796e112f888f5e22
Nitin <ngutta@umich.edu>
2017-12-03 17:25:51 -0500

    this is the front page

---
+++ nitinparts/hellocode/connect.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "Accounts";
+
+$db = new mysqli($servername, $username, $password, $db) or die("Connection failed:");
+
+?>
